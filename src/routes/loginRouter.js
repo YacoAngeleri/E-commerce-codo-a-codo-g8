7e036a35400e40b75ca7ae9db87622c932a2eb64
emmesshoes <emmesshoes@gmail.com>
2023-12-05 17:40:27 -0300

    Update loginRouter.js

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -23,22 +23,6 @@ loginRouter.get('/login', async (req, res) => {
     res.sendFile(htmlFilePath);
   });
 
-//   loginRouter.post('/login', async (req, res) => {
-//     try {
-//         console.log('Recibiendo solicitud POST en /login');
-//         const { email, contraseña } = req.body;
-//         console.log('Datos del formulario de inicio de sesión:', { email, contraseña });
-        
-//         // Envía el archivo HTML como respuesta
-//         const htmlFilePath2 = path.join(__dirname, '../../public/HTML/productos.html');
-//         res.sendFile(htmlFilePath2);
-       
-//     } catch (error) {
-//         console.error('Error en el inicio de sesión:', error);
-//         res.status(500).json({ success: false, message: 'Error interno del servidor' });
-//     }
-// });
-// export default loginRouter;
 
 loginRouter.post('/login', async (req, res) => {
     try {
@@ -58,12 +42,13 @@ loginRouter.post('/login', async (req, res) => {
         const htmlFilePath = path.join(__dirname, '../../public/HTML/agregar-producto.html');
         res.sendFile(htmlFilePath);
       } else {
+        getCarrito(token);
         const htmlFilePath = path.join(__dirname, '../../public/HTML/productos.html');
         res.sendFile(htmlFilePath);
       }
     } catch (error) {
       console.error('Error en el inicio de sesión:', error);
-      res.status(500).json({ success: false, message: 'Error interno del servidor' });
+      res.status(500).json({ success: false, message: 'Nombre de usuario o Contraseña equivocada' });
     }
   });
   
